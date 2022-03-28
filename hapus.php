f5921da6636477ab159e8897fe9fef9455f74078
vitakarenina06@gmail.com <vitakarenina06@gmail.com>
2022-03-28 15:03:48 +0700

    fixing

--- hapus.php
+++ hapus.php
@@ -4,11 +4,15 @@ require_once('koneksi.php');
 
 $sql = "delete from mahasiswa where nim='" . $_GET['nim'] . "'";
 $result = pg_query($sql);
-$jumlah = pg_num_rows($result);
 
-if($jumlah > 0) {
+if($result) {
     echo '<script type="text/javascript">';
     echo 'alert("Data telah terhapus");';
+    echo 'window.location.href = "index.php";';
+    echo '</script>';
+} else {
+    echo '<script type="text/javascript">';
+    echo 'alert("Data GAGAL terhapus");';
     echo 'window.location.href = "index.php";';
     echo '</script>';
 }
