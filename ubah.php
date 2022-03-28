76b2df016cf603ab36658769e1b94069ce644b3b
vitakarenina06@gmail.com <vitakarenina06@gmail.com>
2022-03-28 15:49:53 +0700

    editing

--- ubah.php
+++ ubah.php
@@ -23,3 +23,19 @@ echo '<input type="text" name="kelas" value="' . $data->kelas . '"><br>';
 ?>
     <input type="submit" value="Simpan">
 </form>
+
+<?php
+if(isset($_POST['nim']) && !empty($_POST['nim'])) {
+    $sql = "update mahasiswa set nama='" . $_POST['nama'] . "', " .
+        "kelas='" . $_POST['kelas'] . "' " .
+        "where nim='" . $_POST['nim'] . "'";
+    $result = pg_query($sql);
+    $row = pg_affected_rows($result);
+    if($row == 1) {
+        echo '<script type="text/javascript">';
+        echo 'alert("Data berhasil diubah");';
+        echo 'window.location.href = "index.php";';
+        echo '</script>';
+    }
+}
+?>
